35b3e4ffa10d02d09d34109e44d697e162110a5a
Decem6er <cgmz@live.com>
2019-12-17 23:43:44 +0800

    Ver 1.0
    Ver 1.0

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Arknights-Auto
 明日方舟 代理
+
+## 依赖
+请到 [Airtest](http://airtest.netease.com/) 官网下载Aritest
+脚本具体使用方法将airtest官方文档
+
+## 说明
+该脚本可实现自动循环刷某一关卡。
+在开始运行前请将游戏界面调至 “开始行动” 界面
---
+++ 明日方舟 代理.py
@@ -0,0 +1,20 @@
+# -*- encoding=utf8 -*-
+__author__ = "Xuyy1"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+t=0
+while t<5:
+    touch(Template(r"tpl1576586836702.png", record_pos=(0.392, 0.227), resolution=(1440, 810)))
+    wait(Template(r"tpl1576586904325.png", record_pos=(0.367, 0.105), resolution=(1440, 810)))
+    touch(Template(r"tpl1576587041414.png", record_pos=(0.362, 0.11), resolution=(1440, 810)))
+
+    touch(wait(Template(r"tpl1576587665895.png", record_pos=(-0.394, -0.128), resolution=(1440, 810)),timeout=30000,interval=3.0))
+    
+    touch((890,340))
+    t+=1
+    sleep(3)
+
+        
+    
