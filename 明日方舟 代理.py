b2a3763df88937812a4046dcdda58fc86007e72d
Decem6er <cgmz@live.com>
2019-12-18 10:53:39 +0800

    ver. 1.1

--- 明日方舟 代理.py
+++ 明日方舟 代理.py
@@ -6,6 +6,7 @@ from airtest.core.api import *
 auto_setup(__file__)
 t=0
 while t<5:
+    t+=1
     touch(Template(r"tpl1576586836702.png", record_pos=(0.392, 0.227), resolution=(1440, 810)))
     wait(Template(r"tpl1576586904325.png", record_pos=(0.367, 0.105), resolution=(1440, 810)))
     touch(Template(r"tpl1576587041414.png", record_pos=(0.362, 0.11), resolution=(1440, 810)))
@@ -13,8 +14,11 @@ while t<5:
     touch(wait(Template(r"tpl1576587665895.png", record_pos=(-0.394, -0.128), resolution=(1440, 810)),timeout=30000,interval=3.0))
     
     touch((890,340))
-    t+=1
+    
     sleep(3)
+
+    if t==5:
+        break
 
         
     
